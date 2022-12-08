2a4c2d52ee43d7e590e6469cfa6418ab456b4bd8
miharyjoel <miharyjoel@gmail.com>
2022-12-08 18:23:09 +0300

    feat:  add mapper

--- src/main/java/app/prog/controller/mapper/BookRestMapper.java
+++ src/main/java/app/prog/controller/mapper/BookRestMapper.java
@@ -1,11 +1,21 @@
 package app.prog.controller.mapper;
 
+import app.prog.controller.request.BookRequest;
+import app.prog.controller.request.BookUpdate;
 import app.prog.controller.response.BookResponse;
 import app.prog.model.Book;
+import app.prog.repository.BookRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+
+
 @Component
+@AllArgsConstructor
 public class BookRestMapper {
+
+    private final BookRepository repository;
+
     public BookResponse toRest(Book domain) {
         return BookResponse.builder()
                 .id(domain.getId())
@@ -13,5 +23,20 @@ public class BookRestMapper {
                 .author(domain.getAuthor())
                 .hasAuthor(domain.hasAuthor())
                 .build();
+    }
+
+    public Book toCreate(BookRequest domain) {
+        return Book.builder()
+                .title(domain.getTitle())
+                .author(domain.getAuthor())
+                .pageNumber(10)
+                .build();
+    }
+
+    public Book toUpdate(BookUpdate data) {
+        Book toUpdate =  repository.getById(String.valueOf(data.getId()));
+        toUpdate.setTitle(data.getTitle());
+        toUpdate.setAuthor(data.getAuthor());
+        return toUpdate;
     }
 }
