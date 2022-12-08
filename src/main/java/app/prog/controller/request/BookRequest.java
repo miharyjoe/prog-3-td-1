1b42896f88626ba755280030c3f513668a412bab
miharyjoel <miharyjoel@gmail.com>
2022-12-08 17:33:48 +0300

    chore:  book request

---
+++ src/main/java/app/prog/controller/request/BookRequest.java
@@ -0,0 +1,15 @@
+package app.prog.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class BookRequest {
+    private String author;
+    private String title;
+}
